3a81b874547b97a7c782c6732d6fd4d547b78524
zhenshuai <zhenshuai@worktile.com>
2016-09-23 16:54:00 +0800

    bugfix

--- dist/angular-wt-genius.js
+++ dist/angular-wt-genius.js
@@ -1,44 +1,8 @@
 angular.module('wt.genius', []);
 /**
- * $wtRetina
- *
- * Version: 1.0.0 - 2015-10-19
- * Anthor: zhenshuai
- */
-(function () {
-    'use strict';
-    angular.module('wt.genius')
-        .provider('$wtRetina', [function () {
-            var defaults = {
-                onchange: function () {
-                    //console.log('dpi 切换事件');
-                }
-            };
-            var configOptions = {};
-            this.config = function (value) {
-                configOptions = value;
-            };
-            this.$get = [
-                function () {
-                    var mediaQuery = "(min--moz-device-pixel-ratio: 1.5),\
-                                        (-o-min-device-pixel-ratio: 3/2),\
-                                        (-webkit-min-device-pixel-ratio: 1.5),\
-                                        (min-device-pixel-ratio: 1.5),\
-                                        (min-resolution: 144dpi),\
-                                        (min-resolution: 1.5dppx)";
-
-                    var matchObj = window.matchMedia(mediaQuery);
-                    return {
-                        isRetina: matchObj.matches,
-                        media   : matchObj.media
-                    };
-                }
-            ];
-        }]);
-})();
-/**
  * $wtNotify
  *
+ * Version: 1.1.0 - 2016-09-23
  * Version: 1.0.0 - 2015-08-25
  * Anthor: zhenshuai
  */
@@ -115,5 +79,42 @@ angular.module('wt.genius', []);
                 };
                 return result;
             }];
+        }]);
+})();
+/**
+ * $wtRetina
+ *
+ * Version: 1.0.0 - 2015-10-19
+ * Anthor: zhenshuai
+ */
+(function () {
+    'use strict';
+    angular.module('wt.genius')
+        .provider('$wtRetina', [function () {
+            var defaults = {
+                onchange: function () {
+                    //console.log('dpi 切换事件');
+                }
+            };
+            var configOptions = {};
+            this.config = function (value) {
+                configOptions = value;
+            };
+            this.$get = [
+                function () {
+                    var mediaQuery = "(min--moz-device-pixel-ratio: 1.5),\
+                                        (-o-min-device-pixel-ratio: 3/2),\
+                                        (-webkit-min-device-pixel-ratio: 1.5),\
+                                        (min-device-pixel-ratio: 1.5),\
+                                        (min-resolution: 144dpi),\
+                                        (min-resolution: 1.5dppx)";
+
+                    var matchObj = window.matchMedia(mediaQuery);
+                    return {
+                        isRetina: matchObj.matches,
+                        media   : matchObj.media
+                    };
+                }
+            ];
         }]);
 })();
